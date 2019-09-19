e29a409e37b2e7ffafe5b2f66dab7e142efb07c0
JJ <justinjcode@gmail.com>
2019-09-19 04:44:33 -0700

    create-react-app scaffolding

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './app.css';
+
+import HeroSelection from './components/HeroSelection';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <HeroSelection />
+    </div>
+  );
+}
+
+export default App;
